d92a0e696127dd8faacc2be2fd9d155da322348f
sw-yx <swyx@dontemail.me>
2019-08-27 15:18:05 -0400

    env up

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+process.env.colors = true
 const chalk = require('chalk')
 
 console.log(chalk.blue('this is blue'))
 console.log(`supportsColor: ${JSON.stringify(chalk.supportsColor)}`)
-
-process.env.colors = true
 
 console.log(chalk.yellow('this is yellow'))
 console.log(`supportsColor: ${JSON.stringify(chalk.supportsColor)}`)
